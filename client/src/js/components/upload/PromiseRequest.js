380ab88c8cecafbb235e4693fbd909743b246ea7
Elger van Boxtel <elgervb@users.noreply.github.com>
2016-10-13 17:14:23 +0200

    library to wrap XHR in promise

---
+++ client/src/js/components/upload/PromiseRequest.js
@@ -0,0 +1,36 @@
+
+
+export default class PromiseRequest {
+
+    get(url, data) {
+        return this.request('GET', url, data);
+    }
+
+    post(url, data) {
+        return this.request('POST', url, data);
+    }
+
+    request(method, url, data) {
+        return new Promise((resolve, reject) => {
+            const xhr = new XMLHttpRequest();
+            
+            xhr.open(method, url);
+            
+            xhr.onload = () => {
+                if (xhr.status === 200) {
+                    resolve(xhr);
+                } else {
+                    reject(Error(`XMLHttpRequest failed; error code: ${xhr.statusText}`));
+                }
+            };
+
+            xhr.onerror = reject;
+
+            if (data) {
+                xhr.send(data);
+            } else {
+                xhr.send();
+            }
+        });
+    }
+}
