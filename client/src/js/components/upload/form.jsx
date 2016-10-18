2a27d2f95f261bd749179375e2fc26e7c7c2e53d
Elger van Boxtel <e.vanboxtel@hw986-evanboxtel.local>
2016-10-18 13:55:31 +0200

    #4 show image preview prior to upload fixed #4

--- client/src/js/components/upload/form.jsx
+++ client/src/js/components/upload/form.jsx
@@ -10,16 +10,19 @@ export default class UploadForm extends React.Component {
             error: '',
         };
 
-        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleUpload = this.handleUpload.bind(this);
     }
 
-    handleSubmit() {
-        const formdata = new FormData(document.querySelector('.uploadForm'));
+    handleUpload(evt) {
+        debugger;
+        const formdata = new FormData(document.querySelector('.uploadform'));
         const request = new Request();
         const success = (data) => {
             this.props.onUpload(data);
             this.setState({ error: '' });
         };
+
+        this.showPreview(evt);
 
         request.post('http://localhost:4001/upload', formdata)
         .then(success.bind(this), (err) => {
@@ -28,12 +31,51 @@ export default class UploadForm extends React.Component {
         });
     }
 
+    showPreview(evt) {
+        if (window.File && window.FileReader && window.FileList && window.Blob) {
+            const files = evt.target.files;
+            let file, i;
+
+            for (i = 0; i < files.length; i++) {
+                file = files[i];
+
+                if (file.type.match('image.*')) {
+                    const reader = new FileReader();
+                    /* eslint-disable arrow-body-style */
+                    //
+                    reader.onload = ((f) => {
+                        return (e) => {
+                            this.setState({
+                                image: {
+                                    src: e.target.result,
+                                    name: f.name,
+                                },
+                            });
+                        };
+                    })(file);
+                    /* eslint-enable arrow-body-style */
+
+                    reader.readAsDataURL(file);
+                }
+            }
+        }
+    }
+
     render() {
         return (
-            <form className="uploadForm" method="post" encType="multipart/form-data"> 
-                <div className="error">{this.state.error}</div>
-                <input type="file" name="upload" id="upload" required onChange={this.handleSubmit} />
-            </form>
+            <div className="overlay overlay--slidein">
+                <form className="uploadform" method="post" encType="multipart/form-data">
+                    <h1 className="uploadform__header">Upload your image</h1>
+                    <div className="error">{this.state.error}</div>
+                    <input type="file" name="upload" id="upload" required onChange={this.handleUpload} />
+                    <label htmlFor="upload" className="uploadform__button">Upload</label>
+
+                    <div className="preview">
+                        <p>{(this.state.image) ? this.state.image.name : ''}</p>
+                        <img className="preview__thumb" src={(this.state.image) ? this.state.image.src : ''} />
+                    </div>
+                </form>
+            </div>
         );
     }
 }
