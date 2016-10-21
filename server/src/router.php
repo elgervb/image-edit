dee9e5b44bfd4b70c53f294f2fe3cbf125a1136e
Elger van Boxtel <e.vanboxtel@hw986-evanboxtel.local>
2016-10-21 22:51:16 +0200

    all resources should go through index.php

--- server/src/router.php
+++ server/src/router.php
@@ -1,8 +1,8 @@
 <?php
 // router.php
-$isUploadedImage = preg_match('#^/image/uploads.*\.(?:png|jpg|jpeg|gif)$#', $_SERVER["REQUEST_URI"]);
-if (!$isUploadedImage && preg_match('/\.(?:png|jpg|jpeg|gif)$/', $_SERVER["REQUEST_URI"])) {
-	return false;    // serve the requested resource as-is.
-} else {
+// $isUploadedImage = preg_match('#^/image/uploads.*\.(?:png|jpg|jpeg|gif)$#', $_SERVER["REQUEST_URI"]);
+// if (!$isUploadedImage && preg_match('/\.(?:png|jpg|jpeg|gif)$/', $_SERVER["REQUEST_URI"])) {
+// 	return false;    // serve the requested resource as-is.
+// } else {
 	include('index.php');
-}
+// }
